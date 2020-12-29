c600f97fca3f02925f1797b07dcfe1b92013141f
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2020-12-29 11:26:47 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authecticate_user!
+  before_action :configure_permited_parametaers, if: :devise_controller?
+
+  private
+  def configure_permited_parametaers
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 end
