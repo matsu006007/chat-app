0214bfef2c1f1a0b22bbb8e5eac119fa33e3c2a2
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2020-12-29 12:54:09 +0900

    スペルミスの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authecticate_user!
-  before_action :configure_permited_parametaers, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-  def configure_permited_parametaers
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
